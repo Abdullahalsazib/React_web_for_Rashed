7332373d06313a66882d22b8acaaf562f47dac5f
Abdullahalsazib <mdsazibhossin2021@gmail.com>
2023-11-18 00:49:29 +0600

    first commit2

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,16 @@ import {
   HashRouter as Router,
   Route,
   Routes,
-  BrowserRouter,
-  Link,
-  Outlet,
   HashRouter,
 } from "react-router-dom";
 import Navbar from "./Components/Pages/Navbar";
 import Join from "./Components/Pages/Join";
-import { motion } from "framer-motion";
 
 function App() {
   return (
     <div className=" relative bg-slate-25 dark:bg-dark-800">
-      {/* <motion.div
-        initial={{ x: 0 }}
-        animate={{ x: "-100% "}}
-        transition={{ duration: 2 }}
-        className=" bg-slate-50 w-[100vw] top-0 py-10 absolute"
-      ></motion.div> */}
-  
       <Navbar />
-      <HashRouter>
+      <Router>
         <Routes>
           <Route index path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -35,7 +24,7 @@ function App() {
           <Route path="/join" element={<Join />} />
           <Route path="*" element={<NoPage />} />
         </Routes>
-      </HashRouter>
+      </Router>
     </div>
   );
 }
